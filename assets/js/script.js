b91dfd20387d50011fe06ee9f13e1400dc10e3c5
jkp1990 <jessicapourawal@gmail.com>
2024-01-09 22:41:35 +0000

    updated javascript file and html so the event can be saved in local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,10 @@
-// Dislay the current day at the top of the calender when a user opens the planner.
-
+// dislay the current day at the top of the calender when a user opens the planner.
 function displayTime() {
   $("#currentDay").text(dayjs().format("dddd MMMM D"));
 }
-
 setInterval(displayTime, 1000);
 
-// Color-code each timeblock based on past, present, and future when the timeblock is viewed
-
+// color-code each timeblock based on past, present, and future when the timeblock is viewed
 function updateColors() {
   var currentTime = dayjs();
   // Select all elements with the class 'timeBlock'
@@ -27,12 +24,34 @@ function updateColors() {
   });
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-  updateColors();
-});
-
-// Allow a user to enter an event when they click a timeblock
+// all the save buttons
+var saveButtons = document.querySelectorAll(".saveBtn");
 
 // Save the event in local storage when the save button is clicked in that timeblock.
+saveButtons.forEach((btn) => {
+  btn.addEventListener("click", function (event) {
+    const id = btn.getAttribute("id");
+    const text = document.getElementById(`${id}-textarea`).value;
+
+    localStorage.setItem(id, text);
+  });
+});
 
 // Persist events between refreshes of a page
+function getEvents() {
+  saveButtons.forEach((btn) => {
+    const id = btn.getAttribute("id");
+
+    const text = localStorage.getItem(id);
+
+    var textarea = document.getElementById(`${id}-textarea`);
+
+    textarea.value = text;
+  });
+}
+
+// on DOM content loaded - update colours and get events from the calender
+document.addEventListener("DOMContentLoaded", function () {
+  updateColors();
+  getEvents();
+});
